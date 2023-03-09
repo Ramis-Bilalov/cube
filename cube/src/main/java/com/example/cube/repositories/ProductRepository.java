e77d8c974c96e7640ba21c51f8070ca767dbc7ca
RamisDeveloper <ramiska81@gmail.com>
2023-03-09 23:18:38 +0300

    добавлена бд

---
+++ cube/src/main/java/com/example/cube/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.example.cube.repositories;
+
+import com.example.cube.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByTitle(String title);
+
+
+}
